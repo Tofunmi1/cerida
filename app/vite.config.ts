4ec86eecc3966bd5d8617a31170c8ce7f2fd9757
Adediran Tofunmi <folakemiadediran14@gmail.com>
2026-07-03 18:53:25 +0100

    chore: orderbook render, trades tape, keeper build updates

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/tee': {
-        target: 'http://35.232.138.136:9721',
+        target: 'http://35.255.76.255:9721',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/tee/, ''),
       },
